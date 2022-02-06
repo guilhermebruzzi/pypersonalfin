171900daf4bed1e264232a80c5bdbe28dfcf42a6
Guilherme Bruzzi <guibruzzi@gmail.com>
2022-02-05 23:02:42 -0300

    Add parser middleware capabilities

--- pypersonalfin/scrapper.py
+++ pypersonalfin/scrapper.py
@@ -9,6 +9,16 @@ from utils.locale import is_brazil
 from utils.amount import amount_to_str
 from utils.file import get_files_of_data_folder, get_file_content
 
+middleware_change_parser_categories = None
+try:
+    from scripts.scrapper import change_parser_categories
+
+    print('Custom parser categories middleware')
+
+    middleware_change_parser_categories = change_parser_categories
+except:
+    print('No custom parser categories middleware')
+
 file_contents = None
 
 
@@ -40,7 +50,10 @@ def _scrapper_parser(parser, date_begin, date_end):
         if parser.match(name):
             file_contents_of_parser.append(file_content)
 
-    return parser.get_categories(file_contents_of_parser, date_begin, date_end)
+    parser_categories = parser.get_categories(
+        file_contents_of_parser, date_begin, date_end)
+
+    return middleware_change_parser_categories(parser.name, parser_categories) if middleware_change_parser_categories else parser_categories
 
 
 def _get_file_description(lower_bound_date, upper_bound_date, locale):
@@ -62,14 +75,12 @@ def _get_file_name_from_description(file_description):
     return slugify(file_description)
 
 
-def scrapper(parserclasses, locale, date_begin, date_end, dates_per_parser):
-    if not parserclasses or len(parserclasses) == 0:
-        return
-
+def _get_categories_per_parser(parserclasses, locale, date_begin, date_end, dates_per_parser):
     categories_per_parser = defaultdict(list)
     amount = 0
     lower_bound_date = None
     upper_bound_date = None
+
     for parsercls in parserclasses:
         parser = parsercls(locale)
         parser_dt_begin = date_begin
@@ -93,15 +104,13 @@ def scrapper(parserclasses, locale, date_begin, date_end, dates_per_parser):
 
             categories_per_parser[parser.name].append(category)
 
-    file_description = _get_file_description(
-        lower_bound_date, upper_bound_date, locale
-    )
-    file_name = _get_file_name_from_description(file_description)
+    return categories_per_parser, amount, lower_bound_date, upper_bound_date
 
-    csv = "{}\n".format(file_description)
 
+def _get_csv_from_categories_per_parser(categories_per_parser, amount, locale):
     income_category_name = Category.get_default_category_name(1, locale)
     exit_category_name = Category.get_default_category_name(-1, locale)
+    csv = ''
 
     for parser_name, categories in categories_per_parser.items():
         categories.sort(key=lambda c: datetime.date(2000, 1, 1) if c.amount >
@@ -159,5 +168,27 @@ def scrapper(parserclasses, locale, date_begin, date_end, dates_per_parser):
         csv += "\nsaldo final;{}\n".format(amount_to_str(amount, locale))
     else:
         csv += "\ntotal balance;{}\n".format(amount_to_str(amount, locale))
+
+    return csv
+
+
+def scrapper(parserclasses, locale, date_begin, date_end, dates_per_parser):
+    if not parserclasses or len(parserclasses) == 0:
+        return
+
+    categories_per_parser, amount, lower_bound_date, upper_bound_date = _get_categories_per_parser(
+        parserclasses, locale, date_begin, date_end, dates_per_parser
+    )
+
+    file_description = _get_file_description(
+        lower_bound_date, upper_bound_date, locale
+    )
+    file_name = _get_file_name_from_description(file_description)
+
+    csv = "{}\n".format(file_description)
+
+    csv += _get_csv_from_categories_per_parser(
+        categories_per_parser, amount, locale
+    )
 
     return (csv, file_name)
--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,5 @@
 # Scripts folder
 
 This folder can contains any script that you want to personal insert in this project without commiting and pushing to github (extensions ignored: .js, .ts, .py and .sh )
+
+If you declare special module named `scrapper.py` with `def change_parser_categories(parser_name, parser_categories)` it will be passed the list of categories statements to change before saving (filter and group values).
